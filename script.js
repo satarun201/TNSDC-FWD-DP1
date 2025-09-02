a0c77c047e90c50325ab23096105c2dae26adf96
satarun201 <satarun201@gmail.com>
2025-09-02 21:22:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Script for adding interactivity
+document
+  .querySelector(".contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Message Sent Successfully!");
+  });
+a;
