e2a1b5054f926c9d6cfd45d25c147acf58bf991e
satarun201 <satarun201@gmail.com>
2025-09-02 21:14:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Script for adding interactivity
-document
-  .querySelector(".contact-form")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Message Sent Successfully!");
-  });
-a;
